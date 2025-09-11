5eff98a14cb7716fdbce6bc423d64d8a672afaf6
Johan s <48106430+johan12361@users.noreply.github.com>
2025-09-11 17:12:13 -0500

    update data articlesDaily

--- src/ai/openAI/buildPrompt/articlesDaily.mjs
+++ src/ai/openAI/buildPrompt/articlesDaily.mjs
@@ -2,12 +2,14 @@ import { getArticlesDaily } from '#config/articles/articlesDaily.mjs'
 
 //TT CONSTRUIR ARTÍCULOS
 export async function buildArticlesDaily() {
+  console.log('Construyendo artículos diarios...')
   const articles = await getArticlesDaily()
   if (!articles) {
     return 'No hay artículos diarios disponibles'
   }
-  const activeArticles = articles.filter((article) => article.availableToday)
+  const activeArticles = articles.filter((article) => article.available)
   if (activeArticles.length === 0) {
+    console.warn('No hay artículos diarios activos disponibles')
     return 'No hay artículos diarios activos disponibles'
   }
 
@@ -16,12 +18,28 @@ export async function buildArticlesDaily() {
   let text = ''
   for (const article of activeArticles) {
     text += `- ${article.description}\n`
+    if (article.advancedDescription && article.advancedDescription.length > 0) {
+      text += `  * Descripción: ${article.advancedDescription}\n`
+    }
     text += `  * Código: ${article.code}\n`
     text += `  * Precio de venta: ${article.salePrice}\n`
     text += `  * Familia: ${article.family}\n`
     text += `  * Grupo: ${article.group}\n`
-    text += `  * Imagen del artículo: ${article.urlImage || 'No disponible'}\n`
-    text += `\n`
+    text += `  * Libre de azúcar: ${article.withoutSugar ? 'Sí' : 'No'}\n`
+    text += `  * Apto para celíacos: ${article.suitableForCeliacs ? 'Sí' : 'No'}\n`
+    text += `  * Libre de harina: ${article.withoutFlour ? 'Sí' : 'No'}\n`
+    text += `  * Vegano: ${article.vegan ? 'Sí' : 'No'}\n`
+
+    if (article.minimumQuantity && article.minimumQuantity > 0) {
+      text += `  * Cantidad mínima: ${article.minimumQuantity}\n`
+    }
+    if (article.multipleOf && article.multipleOf > 0) {
+      text += `  * Cantidad múltiple de: ${article.multipleOf}\n`
+    }
+    if (article.hoursInAdvance && article.hoursInAdvance > 0) {
+      text += `  * Horas de anticipación: ${article.hoursInAdvance}\n`
+    }
+    text += `  * Imagen del artículo: ${article.urlImage || 'No disponible'}\n\n`
   }
   //console.info('appsheet: configuración de <articlesDaily> construida: ', text)
   return text
